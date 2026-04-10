f8a256ffe6a7e03b35064bebc54830750ce65a89
SriramV1212 <sriramjr12@gmail.com>
2026-04-09 22:17:35 -0400

    minor sql schema change

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,9 @@ CREATE TABLE IF NOT EXISTS processed_events (
     event_id TEXT PRIMARY KEY
 );
 
+ALTER TABLE processed_events
+ADD CONSTRAINT unique_event_id UNIQUE (event_id);
+
 
 -- Metrics table to track the total number of processed events for monitoring purposes 
 
